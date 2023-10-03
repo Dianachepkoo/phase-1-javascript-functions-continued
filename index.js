7cd2798ef73d30d54007f6c6c80d64149440562f
Diana Chepkoech <dianachepkoech336@gmail.com>
2023-10-03 11:41:13 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+  }
+  
+  const mondayWork = function(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;
+  };
+  
+  function wrapAdjective(emphasis = "*") {
+    return function(adjective) {
+      return `You are ${emphasis}${adjective}${emphasis}!`;
+    };
+  }
+  
+  // Export the functions if needed
+  module.exports = {
+    saturdayFun,
+    mondayWork,
+    wrapAdjective,
+  };
+  
